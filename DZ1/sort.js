63b118ee7bf6537795ea4aa37949d7563a95d488
Alex Kuimov <spoot@bk.ru>
2021-06-30 12:13:46 +0300

    Rename file

--- DZ1/sort.js
+++ DZ1/sort.js
@@ -3,33 +3,36 @@ const path = require('path');
 
 const output = path.join(__dirname, 'output');
 
-
 const sortFiles = (input, level) => {
-    const files = fs.readdirSync(input);
-  
-    files.forEach(item => {
-      let localBase = path.join(input, item);
-      let state = fs.statSync(localBase);
+    try {
+        const files = fs.readdirSync(input);
 
-      if (state.isDirectory()) {
-        sortFiles(localBase, level + 1);
-      } else {
-        const folderName = item.charAt(0);
-        const folderPath = path.join(output, folderName);
-        const fileFrom = localBase;
-        const fileTo = path.join(folderPath, item);
+        files.forEach(item => {
+            let localBase = path.join(input, item);
+            let state = fs.statSync(localBase);
 
-        fs.mkdir(folderPath, { recursive: true }, (err) => {
-            if (err) throw err;
-            console.log('Directory %s was created', folderName);
+            if (state.isDirectory()) {
+                sortFiles(localBase, level + 1);
+            } else {
+                const folderName = item.charAt(0);
+                const folderPath = path.join(output, folderName);
+                const fileFrom = localBase;
+                const fileTo = path.join(folderPath, item);
 
-            fs.copyFile(fileFrom, fileTo, (err) => {
-                if (err) throw err;
-                console.log('File %s was copied', item);
-            });
-        });
-      }
-    })
+                fs.mkdir(folderPath, { recursive: true }, (err) => {
+                    if (err) throw err;
+                    console.log('Directory %s was created', folderName);
+
+                    fs.copyFile(fileFrom, fileTo, (err) => {
+                        if (err) throw err;
+                        console.log('File %s was copied', item);
+                    });
+                });
+            }
+        })
+    } catch (e) {
+        console.error('outer', e.message);
+    }  
 }
 
 module.exports = sortFiles;
