711a69b1267598c896dafea0b750999050dc4301
Alex Kuimov <spoot@bk.ru>
2021-06-30 12:01:02 +0300

    Add module

--- DZ1/index.js
+++ DZ1/index.js
@@ -1,40 +1,6 @@
-const fs = require('fs');
 const path = require('path');
+const sortFiles = require('./sort');
 
 const input = path.join(__dirname, 'input');
-const output = path.join(__dirname, 'output');
-
-
-const progressInfo = (err) => {
-    if (err) throw err;
-    console.log('source.txt was copied to destination.txt');
-}
-
-
-const sortFiles = (input, level) => {
-    const files = fs.readdirSync(input);
   
-    files.forEach(item => {
-      let localBase = path.join(input, item);
-      let state = fs.statSync(localBase);
-
-
-      if (state.isDirectory()) {
-        sortFiles(localBase, level + 1);
-      } else {
-        const folderName = item.charAt(0);
-        const folderPath = path.join(output, folderName);
-        const fileFrom = localBase;
-        const fileTo = path.join(folderPath, item);
-
-        fs.mkdir(folderPath, { recursive: true }, (err) => {
-            if (err) throw err;
-            fs.copyFile(fileFrom, fileTo, progressInfo);
-        });
-
-      }    
-
-    })
-  }
-  
-  sortFiles(input, 0);
+sortFiles(input, 0);
---
+++ DZ1/sort.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = path.join(__dirname, 'output');
+
+
+const sortFiles = (input, level) => {
+    const files = fs.readdirSync(input);
+  
+    files.forEach(item => {
+      let localBase = path.join(input, item);
+      let state = fs.statSync(localBase);
+
+      if (state.isDirectory()) {
+        sortFiles(localBase, level + 1);
+      } else {
+        const folderName = item.charAt(0);
+        const folderPath = path.join(output, folderName);
+        const fileFrom = localBase;
+        const fileTo = path.join(folderPath, item);
+
+        fs.mkdir(folderPath, { recursive: true }, (err) => {
+            if (err) throw err;
+            console.log('Directory %s was created', folderName);
+
+            fs.copyFile(fileFrom, fileTo, (err) => {
+                if (err) throw err;
+                console.log('File %s was copied', item);
+            });
+        });
+      }
+    })
+}
+
+module.exports = sortFiles;
