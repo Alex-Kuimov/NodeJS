20d66d9bdc62ac6d22360754d839d1c196c8f2af
Alex Kuimov <spoot@bk.ru>
2021-07-03 13:20:09 +0300

    start DZ2

--- DZ2/index.js
+++ DZ2/index.js
@@ -15,23 +15,15 @@ app.get("/date", (req, res, next) => {
 
 let tick = 0;
 setTimeout(function run() { 
-  const now = 'server';  
-
-  //console.log(now);
   tick++;
 
   console.log(tick);
   if (tick >= LIMIT) {
 
-    console.log('end');
     connections.map(res => {
       res.write("END\n");
       res.end();
     });
-
-    /*connections.map((res, i) => {
-      res.write(`Hello ${i}! Tick: ${tick}.\n`);
-    });*/
 
     connections = [];
     tick = 0;
@@ -42,6 +34,6 @@ setTimeout(function run() {
   setTimeout(run, DELAY);
 }, DELAY);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on post ${PORT}`);
 });
