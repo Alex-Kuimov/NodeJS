349634ef15edbb2cf01f2670bb0ddce84c89c5ac
Alex Kuimov <spoot@bk.ru>
2021-07-05 17:13:34 +0300

    fix async

--- DZ1/sort.js
+++ DZ1/sort.js
@@ -5,16 +5,9 @@ const util = require("util");
 const readdir = util.promisify(fs.readdir);
 const mkdir = util.promisify(fs.mkdir);
 const rename = util.promisify(fs.rename);
+const rmdir = util.promisify(fs.rmdirSync);
 
 function mkDir(folderPath, folderName){
-    /*return new Promise(resolve => {
-        fs.mkdir(folderPath, { recursive: true }, (err) => {
-            if (err) throw err;
-            console.log('Directory %s was created', folderName);
-            resolve('Directory %s was created', folderName);
-        });
-    });*/
-
     return mkdir(folderPath, { recursive: true }).then(() => {
         console.log('Directory %s was created', folderName);
     })
@@ -23,20 +16,8 @@ function mkDir(folderPath, folderName){
     });
 }
 
-
 function moveFile(fileFrom, fileTo, file){
-    /*return new Promise(resolve => {
-        fs.access(fileFrom, fs.constants.F_OK, (err) => {
-            if (err) throw err
-            fs.rename(fileFrom, fileTo, function (err) {
-                if (err) throw err
-                console.log('File %s was copied', file);
-                resolve('File %s was copied', file);
-            });
-        });       
-    });*/
-
-    fs.access(fileFrom, fs.constants.F_OK, (err) => {
+    fs.access(fileFrom, fs.constants.F_OK, async (err) => {
         return rename(fileFrom, fileTo).then(() => {
             console.log('File %s was copied', file);
         })
@@ -48,7 +29,6 @@ function moveFile(fileFrom, fileTo, file){
 
 async function sortFiles(input, output, clear) {
     try {
-        //const files = fs.readdirSync(input);
         const files = await readdir(input);
 
         for(const file of files) {
@@ -69,7 +49,7 @@ async function sortFiles(input, output, clear) {
         }
 
         if(clear) {
-            fs.rmdirSync(input);
+            await rmdir(input);
         }
 
     } catch (e) {
