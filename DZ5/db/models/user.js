76033ace9a95b09b89a182b1240317d5bbb3519b
Alex Kuimov <spoot@bk.ru>
2021-07-17 18:56:48 +0300

    add db config

---
+++ DZ5/db/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    userName: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    middleName: DataTypes.STRING,
+    surName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
