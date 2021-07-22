5a176a1602c692010a99303d342a35d948158582
Alex Kuimov <spoot@bk.ru>
2021-07-22 21:14:22 +0300

    add reg, login and delete user

--- DZ5/api/users.js
+++ DZ5/api/users.js
@@ -1,12 +1,75 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const passport = require('passport');
 
 const User = require('../db').models.user;
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
+router.get('/users/', async (req, res, next) => {
   try {
     const result = await User.findAll();
+    res.json({success: true, data: result});
+  }
+  catch(err) {
+    console.error(err);
+    res.json({success: false, err});
+  }
+});
+
+router.post('/registration/', async (req, res, next) => {
+  try {
+    const { userName, surName, firstName, middleName, password } = req.body;
+    const data = {
+      userName,
+      surName,
+      firstName,
+      middleName,
+      password
+    };
+
+    const result = await User.create(data);
+    res.json({success: true, data: result});
+  }
+  catch(err) {
+    console.error(err);
+    res.json({success: false, err});
+  }
+});
+
+router.post('/login/', async (req, res, next) => {
+  try {
+    const { userName, password } = req.body;
+
+    const user = {
+      userName,
+      password,
+    }
+    
+    passport.authenticate("local", (err, user) => {
+      if(err){
+        return next(err)
+      }
+      if(!user){
+        return res.send("Wrong email or password")
+      }
+      req.login(user, () => {
+        res.json({success: true, data: user});
+      })
+    })(req, res, next)
+
+  }
+  catch(err) {
+    console.error(err);
+    res.json({success: false, err});
+  }
+});
+
+router.delete('/users/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params;
+
+    const result = await User.destroy({where: {id}});
     res.json({success: true, data: result});
   }
   catch(err) {
--- DZ5/app.js
+++ DZ5/app.js
@@ -1,7 +1,81 @@
 const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const path = require('path');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
+const User = require('./db').models.user;
 
 const app = express();
+
+app.use(
+	session({
+		store: new FileStore(),
+		secret: "secret",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const getUserbyName = async function (userName) {
+    const user = await User.findOne({
+      where: {
+        userName: userName
+      }
+    });
+
+    return user.dataValues;
+};
+
+const getUserbyID = async function (id) {
+  const user = await User.findOne({
+    where: {
+      id: id
+    }
+  });
+
+  return user.dataValues;
+};
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser( async (id, done) => {
+  const user = await getUserbyID(id);
+  const _user = user.id === id ? user : false;
+  done(null, _user)
+})
+
+passport.use(new LocalStrategy({
+  usernameField: "userName"
+}, async (userName, password, done) => {
+
+    console.log(userName);
+
+    const user = await getUserbyName(userName);
+
+    console.log(user);
+
+  if(userName === user.userName && password === user.password){
+    return done(null, user)
+  }else{
+    return done(null, false)
+  }
+}))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
