c4ace862dd739c67ed68905e50cb0277c77054fe
Alex Kuimov <spoot@bk.ru>
2021-08-04 19:16:01 +0300

    final

---
+++ DZ5/build/precache-manifest.b303b74b64302747c44d10beca9bd579.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a7113e5719845117d7be6187a6d0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7334ef67555645e8b360",
+    "url": "/static/js/2.6319b831.chunk.js"
+  },
+  {
+    "revision": "ebb8a00de6c1ca243590",
+    "url": "/static/js/main.c818d8e1.chunk.js"
+  },
+  {
+    "revision": "32996fb276b3e890a01d",
+    "url": "/static/js/runtime~main.01574ed8.js"
+  }
+]);
